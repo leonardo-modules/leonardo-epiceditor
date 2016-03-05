6ead1ead7b2d75167235f97070d3b20f9f9e1e32
Michael Kutý <6du1ro.n@gmail.com>
2016-03-05 12:37:28 +0100

    Support public editor.

--- leonardo_epiceditor/views.py
+++ leonardo_epiceditor/views.py
@@ -1,11 +1,25 @@
 
+from constance import config
 from django.http import JsonResponse
+from django.utils.decorators import method_decorator
 from django.views import generic
+from django.views.decorators.csrf import csrf_exempt
+from django.middleware.csrf import CsrfViewMiddleware
 
 from .utils import DEFAULT_MARKUP_TYPES
 
 
 class EpicEditorView(generic.TemplateView):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+
+        if not config.LEONARDO_EPICEDITOR_PUBLIC:
+            reason = CsrfViewMiddleware().process_view(request, None, (), {})
+            if reason:
+                return JsonResponse({})
+
+        return super(EpicEditorView, self).dispatch(request, *args, **kwargs)
 
     def get(self, *args, **kwargs):
         return JsonResponse({})
