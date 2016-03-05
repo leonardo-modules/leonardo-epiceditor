ef02662ac09f422f6d93081560e81b53f24154f0
Michael Kutý <6du1ro.n@gmail.com>
2016-03-05 11:05:05 +0100

    Fix urls.

--- leonardo_epiceditor/__init__.py
+++ leonardo_epiceditor/__init__.py
@@ -7,11 +7,11 @@ default_app_config = 'leonardo_epiceditor.Config'
 LEONARDO_APPS = ['leonardo_epiceditor']
 LEONARDO_JS_FILES = ['lib/js/epiceditor.init.js']
 
-
 LEONARDO_OPTGROUP = 'EpicEditor'
+LEONARDO_PUBLIC = True
 
 LEONARDO_CONFIG = {
-    'LEONARDO_EPICEDITOR_PUBLIC': (True, 'Make epiceditor view accessible without login.'),
+    'LEONARDO_EPICEDITOR_PUBLIC': (False, 'Make epiceditor view accessible without login.'),
 }
 
 
